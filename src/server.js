a418a35f3b6327f61b1ab491f3f849b270b51d71
huhq <fox2081@foxmail.com>
2018-08-03 14:57:03 +0800

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,10 @@
+let WebSocketServer = require('ws').Server;
+
+let wss = new WebSocketServer({port: 14001});
+
+wss.on('connection', (ws) => {
+    console.log('Client connected: ', ws);
+    ws.on('message', (msg) => {
+        console.log('Message received: ', msg);
+    });
+});
