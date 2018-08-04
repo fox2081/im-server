1727dd158a076abf68b943570fbe1a67e57ed63d
huhq <huhq@itdayang.com>
2018-08-04 21:29:12 +0800

    [Feature]: Update

--- src/server.js
+++ src/server.js
@@ -3,8 +3,10 @@ let WebSocketServer = require('ws').Server;
 let wss = new WebSocketServer({port: 14001});
 
 wss.on('connection', (ws) => {
-    console.log('Client connected: ', ws);
+    console.log('Client connected');
     ws.on('message', (msg) => {
         console.log('Message received: ', msg);
     });
+    ws.send('Hello!');
+
 });
