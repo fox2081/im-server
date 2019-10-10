7bc969b411d7a31fb698d5021d8cdf9e3d1768b3
Fox2081 <fox2081@foxmail.com>
2019-10-10 17:39:54 +0800

    ✨[Feature] test

--- src/server.js
+++ src/server.js
@@ -1,10 +1,22 @@
-let WebSocketServer = require('ws').Server;
+let WebSocketServer = require('ws')
 
-let wss = new WebSocketServer({port: 14001});
+let wss = new WebSocketServer.Server({ port: 14001 })
 
-wss.on('connection', (ws) => {
-    console.log('Client connected: ', ws);
-    ws.on('message', (msg) => {
-        console.log('Message received: ', msg);
-    });
-});
+wss.on('connection', ws => {
+  console.log('Client connected: ')
+
+  const loop = setInterval(() => {
+    const data = { code: 'haveCar', data: { tunnelIds: ['02a011513c35405e9adaa5b5e0cee2c2'] } }
+    ws.send(JSON.stringify(data))
+  }, 3000)
+
+  ws.on('message', (msg) => {
+    console.log('Message received')
+  })
+
+  ws.on('close', () => {
+    console.log('Message closed')
+    clearInterval(loop)
+    ws.terminate()
+  })
+})
